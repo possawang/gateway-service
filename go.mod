341b061f6838142cb423a549f126575d97a3e36a
AchmadRifai <v-achmad.rifai@traveloka.com>
2022-04-11 23:12:25 +0700

    Hello world

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/possawang/gateway-service
+
+go 1.18
+
+require (
+	github.com/felixge/httpsnoop v1.0.1 // indirect
+	github.com/gorilla/handlers v1.5.1 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/possawang/go-service-lib-common v0.0.1
+)
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/possawang/go-service-lib-common/routerutils"
+)
+
+func helloWorld(w http.ResponseWriter, _ *http.Request) {
+	w.Write([]byte("Hello world"))
+}
+
+func mdwHelloWorld(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Request hello world from %s\n", r.RemoteAddr)
+		h.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	endpoints := make(map[string]routerutils.Endpoint)
+	endpoints["/"] = routerutils.Endpoint{
+		Execution: helloWorld,
+		Method:    "GET",
+		Mdw:       mdwHelloWorld,
+	}
+	routerutils.StartingService(endpoints)
+}
